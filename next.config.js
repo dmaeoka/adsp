8e6dca8fc21b082865da5c2e8aef325d426dee8a
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2025-08-04 15:11:15 +0100

    fix images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	images: { unoptimized: true },
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'tiles.stadiamaps.com',
+				port: '',
+				pathname: '**',
+			},
+		],
+	},
 	typescript: {
 		// Ignore TypeScript errors during build (not recommended for production)
 		ignoreBuildErrors: true,
